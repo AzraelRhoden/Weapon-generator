30236f13a895bb16ca2212c3d8099ccece4c6a6e
AzraelRhoden <azraelmrhoden@gmail.com>
2023-09-24 17:35:08 -0400

    updated random weapon generator to dragon slayer added 2 classes dragon and dragon builder made fight function that initializes and sets the rules for the fight

--- README.md
+++ README.md
@@ -7,3 +7,14 @@
 7. make main function that prints all in a formatted form
 
 completed 1-5 as well as 7 this was a time sensitive project and subclasses would take too long 
+# Dragon slayer
+1. make origin class called dragon 
+2. give dragon getter functions for name species and type
+3. make dragon builder class 
+4. give dragon builder class functions that randomly select traits for a dragon then put them together into a variable
+5. make fight function that takes the dragon traits a name for the main character and the main characters weapon and then determines how the fight goes
+6. have fight return "You win!" or "you lose..." based on weapon type and dragon type
+1-6 complete 
+added features: player health, dragon damage
+1. player health will go down with each encounter and upon a victory will be incrimented by a random value from 10 to 100
+2. dragon damage will take health away from the player and if the damage is greater than the heros health the message "You died..." will print
--- weapon-generator.py
+++ weapon-generator.py
@@ -1,20 +1,13 @@
 import random
 class Hero:
-    def __init__(self, knight, mage):
+    def __init__(self, knight, mage, health):
         self.knight = knight
         self.mage = mage
-
+        self.health = health
     def get_level():
-        while True:
-            try:
-                level = int(input("Please enter your level: "))
-                if int(level) > 20 or int(level) <= 0:
-                    print("Invalid level")
-                    continue
-                print(f"Your level is {level}")
-                break;
-            except ValueError:
-                print("Please enter valid level")
+       level = random.randint(1,21)
+       print(f"Your level is: {level}\n")
+       return level
 
     def get_class():
         while True:
@@ -30,29 +23,161 @@ class Hero:
             except ValueError:
                 print("Please enter valid class") 
 
-class weapons(Hero):
-    def __init__(self, knight, mage):
-        super().__init__(knight, mage)
+    def get_hero_health():
+        health = random.randint(10,50)
+        return health
     
-Fighter_weapons = ["Greatsword","Longsword","Longbow","Greataxe","Shortbow","Warhammer","Dagger","Crossbow","Shortsword","Rapier","Staff"]
-mage_weapons = ["Wand","Staff","Spellbook"]
-Fighter_effects = ["lightning","hellfire","sharpness","protection","blessing","banishment","smiting","fear","rage","toughness","quickstrike"]
-mage_effects = ["lightning bolt","teleportation","fireball","lightning","necromancy"]
+class Dragon:
+    def __init__(self, name, age,health, damage):
+        self.name = name
+        self.age = age
+        self.health = health
+        self.damage = damage
 
+    def get_age(self):
+        return self.age
+    def health_getter(self):
+        return self.health
+    def damage_getter(self):
+        return self.damage
+
+
+class Dragon_builder(Dragon):
+    def __init__(self, age, health, damage, fly_speed):
+        super().__init__(age,health, damage)
+        self.fly_speed = fly_speed
+
+    def ages():
+        get_ages = ["Ancient", "Adult", "Hatchling"]
+        age = get_ages[random.randrange(0,3)]
+        return age
+    
+    def name():
+        give_name = ["Yvocrog", "Sormailth", "Maka", "Xumrio", "Baesor", "Nidrath", "Mayssoi"]
+        given_name = give_name[random.randrange(0,7)]
+        return given_name
+        
+    def flight():
+        fly_speed = random.randrange(60,200)
+        return fly_speed
+    
+    def get_health():
+        health = random.randint(100,500)
+        return health
+    
+    def get_damage():
+        damage = random.randint(20,200)
+        return damage
+    
+    def get_type():
+        types = ["Fire", "Earth", "Water", "Ice","Grass"]
+        dragon_type = types[random.randrange(0,5)]
+        return dragon_type
+
+def build_dragon():
+    name = Dragon_builder.name()
+    age = Dragon_builder.ages()
+    fly_speed = Dragon_builder.flight()
+    dragon_health = Dragon_builder.get_health()
+    dragon_damage = Dragon_builder.get_damage()
+    dragon_type = Dragon_builder.get_type()
+    print(f"{name} The {age} {dragon_type} Dragon with a fly speed of {fly_speed} a health of {dragon_health} and a damage rating of {dragon_damage}\n")
 
 def main():
-    job = weapons.get_class()
-    weapons.get_level()
+    damage = Dragon_builder.get_damage()
+
+    hero_health = Hero.get_hero_health()
+
+    success = random.randint(10,100)
+
+    Fighter_weapons = ["Greatsword","Longsword","Longbow","Greataxe","Shortbow","Warhammer","Dagger","Crossbow","Shortsword","Rapier","Staff"]
+
+    mage_weapons = ["Wand","Staff","Spellbook"]
+
+    Fighter_effects = ["Lightning","Fire","Druidcraft","Lava","Ice", "Water"]
+
+    mage_effects = ["Druidcraft", "Ice bolt", "Fireball", "Lightning","Necromancy"]
+
+    job = Hero.get_class()
+
+    level = Hero.get_level()
+
+    hero_health = hero_health * level
+
+    weapon_strong_fighter = Fighter_weapons[random.randrange(0,11)]
+
+    weapon_weak_fighter =  Fighter_weapons[random.randrange(7,11)]
+
+    effects_strong_mage = mage_effects[random.randrange(0,5)]
+
+    effects_weak_mage =  mage_effects[random.randrange(0,2)]
+
+    weapon_fighter_effects = Fighter_effects[random.randrange(0,6)]
+
+    mage_weapon = mage_weapons[random.randrange(0,3)]
+    
     if job == "Fighter":
-        stat_select_Fighter = int(input("Please enter a strength score 1-20: "))
+        stat_select_Fighter = random.randint(1,21)
+        print(f"Your stregnth is: {stat_select_Fighter}\n")
         if stat_select_Fighter > 5:
-            print("Fighter: " + Fighter_weapons[random.randrange(0,11)] + " of " + Fighter_effects[random.randrange(0,11)])
+            print(f"Fighter: {weapon_strong_fighter} of {weapon_fighter_effects}\n")
         else:
-            print("Fighter: " + Fighter_weapons[random.randrange(7,11)] + " of " + Fighter_effects[random.randrange(0,11)])
+            print(f"Fighter: {weapon_weak_fighter} of {weapon_fighter_effects}\n")
+
     elif job == "Mage":
-        stat_select_mage = int(input("Pleas enter an int score 1-20: "))
+        stat_select_mage = random.randint(1,21)
+        print(f"Your int is: {stat_select_mage}\n")
         if stat_select_mage > 5:
-            print("Mage: " + mage_weapons[random.randrange(0,3)] + " of " + mage_effects[random.randrange(0,5)])
+            print(f"Mage: {mage_weapon} of {effects_strong_mage} \n")
+
         else:
-            print("Mage: " + mage_weapons[random.randrange(0,3)] + "  of " + mage_effects[random.randrange(0,2)] )
+            print(f"Mage: {mage_weapon} of {effects_weak_mage} \n")
+    fights = 0
+    while hero_health > 0 and fights < 3:
+        print(f"Your health is: {hero_health}\n")
+
+        fight(effects_weak_mage, effects_strong_mage, Fighter_effects)
+        hero_health -= damage
+        print(f"Your health is: {hero_health}\n")
+        
+        fights += 1
+        
+        if hero_health <= 0:
+            print("you died...")
+            break;
+        else:
+            hero_health += success
+            print(f"You win! you regained {success} health!\n")
+            if fights == 3 and hero_health > 0:
+                print("You beat them all!\n")
+                break;
+            continue
+
+
+def fight(effects_weak_mage, effects_strong_mage,Fighter_effects):
+    build_dragon()
+    dragon_type = Dragon_builder.get_type()
+    if dragon_type == "Fire" and Fighter_effects == "Water" or effects_strong_mage == "water":
+        print("You win!\n")
+
+    elif dragon_type == "Fire" and Fighter_effects == "Ice" or effects_strong_mage  == "Ice":
+        print("You lose!\n")
+    
+    elif dragon_type == "Ice" and Fighter_effects == "Fire" or Fighter_effects == "Lava" or effects_strong_mage == "Fireball" or effects_strong_mage == "Lava":
+        print("You win!\n")
+    
+    elif dragon_type == Fighter_effects:
+        print("You lose...\n")
+
+    elif dragon_type == "Water" and Fighter_effects == "Lightning" or Fighter_effects == "Ice" or effects_strong_mage == "Ice bolt" or effects_strong_mage == "Lightning":
+        print("you win!\n")        
+    
+    elif dragon_type == "Grass" and Fighter_effects == "Lightning" or Fighter_effects == "Fire" or Fighter_effects == "Lava" or effects_strong_mage == "Fireball" or effects_strong_mage == "Necromancy":
+        print("You win!\n")
+
+    elif dragon_type == "Earth" and Fighter_effects == "Druidcraft" or effects_strong_mage == "Druidcraft" or effects_weak_mage == [1]:
+        print("You win!\n")        
+    else:
+        print("You died...\n")
+
 main()
